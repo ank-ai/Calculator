d598655b29949b3acfcb11c519cafc5b6b56b796
Ankai Yan <ankai.yan@icloud.com>
2023-03-25 17:26:16 +0000

    Doing this commit in the terminal

--- script.js
+++ script.js
@@ -16,18 +16,21 @@ function divide(arg1, arg2){
 }
 
 function operate(operator,arg1,arg2){
+
     switch (operator){
-        case "add":
+        case "+":
             return add(arg1,arg2);
 
-        case "subtract":
-            return subtract(arg1,arg2);
+        case "-":
+           return subtract(arg1,arg2);
 
-        case "multiply":
-            return multiply(arg1,arg2);
+        case "x":
+           
+           return multiply(arg1,arg2)
         
-        case "divide":
-            return divide(arg1,arg2);
+        case "%":
+         
+           return divide(arg1,arg2)
 
         default: 
         return "Something must have gone wrong in the operate function"
@@ -35,24 +38,62 @@ function operate(operator,arg1,arg2){
     }
 }
 
+function setDisplay(value){
+    display.textContent = value;
+}
+
+function getDisplay(){
+    return Number(display.textContent)
+}
 //-------------------------------------------------variables to update display
 
 let firstNumber;
-let operator;
 let secondNumber;
+let operator;
 let display=document.querySelector(".display");
 
 
 //------------------------------------------------functions that populate display when number buttons are pressed
 
 function concatDisplayValue(){
-    display.textContent+=this.textContent;
+    if(secondNumber==undefined && firstNumber!=undefined){
+        display.textContent= this.textContent; 
+        secondNumber = Number(display.textContent);  
+    }else{display.textContent+=this.textContent;}
 }
 
 const numbers = document.querySelectorAll(".numb");
 numbers.forEach(button=>{button.addEventListener("click",concatDisplayValue)})
 
+//----------------------------------------------
 
+function useOperand(){
 
+    if(firstNumber == undefined){
+        firstNumber =  getDisplay();
+        operator = this.textContent;
 
+    }else{
+        if(secondNumber!=undefined){execute();}
+        operator = this.textContent;    
+    }
+       
+}
 
+const operators = document.querySelectorAll(".operator");
+operators.forEach(button=>{button.addEventListener("click",useOperand)})
+
+//---------------------------------------------
+
+function execute(){
+    secondNumber= Number(display.textContent);
+    let result= operate(operator,firstNumber, secondNumber)
+    setDisplay(result)
+    firstNumber= result;
+    secondNumber = undefined;
+}
+
+//--------------------------------------------------
+
+const equals = document.querySelector(".equals");
+equals.addEventListener("click", execute);
